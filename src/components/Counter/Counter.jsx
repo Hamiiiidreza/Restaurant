a39822e48d67ea52514a9b25b425cd94d3bca1c8
Hamiiiidreza <ha79mi45dd@gmail.com>
2025-08-04 10:49:21 +0330

    feat: Add a landingCounter component to automatically count numbers

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-import Countercard from './Countercard'
+import React from 'react';
+import Countercard from './Countercard';
 
 export default function Counter() {
-
-  const Statistics = [
-    {id:1, number:'210', name:'لحظات عالی'},
-    {id:1, number:'160', name:'لحظات عالی'},
-    {id:1, number:'470', name:'فنجان قهوه روزانه'},
-    {id:1, number:'23', name:'فنجان قهوه'}
-  ]
+  const statistics = [
+    { id: 1, number: '210', name: 'لحظات عالی' },
+    { id: 2, number: '160', name: 'لحظات عالی' },
+    { id: 3, number: '470', name: 'فنجان قهوه روزانه' },
+    { id: 4, number: '23', name: 'فنجان قهوه' }
+  ];
 
   return (
     <div className='statistics relative pt-[130px] pb-[100px]'>
-      <img className='absolute top-0 right-0 w-full h-full -z-10' src="../Public/Img/bg-shape-Statistics.webp" alt="bgshape" />
+      <img
+        className='absolute top-0 right-0 w-full h-full -z-10'
+        src="../Public/Img/bg-shape-Statistics.webp"
+        alt="bgshape"
+      />
       <div className='container'>
         <div className='counter-card-wrap flex z-10'>
-          <Countercard {...Statistics[0]} />
-          <Countercard {...Statistics[1]} />
-          <Countercard {...Statistics[2]} />
-          <Countercard {...Statistics[3]} />
+          {statistics.map(stat => (
+            <Countercard
+              key={stat.id}
+              number={stat.number}
+              name={stat.name}
+            />
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Counter/Countercard.jsx
+++ src/components/Counter/Countercard.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import LandingCounter from '../LandingCounter/LandingCounter';
 
 export default function Countercard({ number, name }) {
     return (
         <div className='counter-card w-[25%] mb-[30px] pr-[30px]'>
             <h4 className='text-orange-250 mb-[10px] text-8xl font-bold leading-[96px]'>
-                <span>{number}</span>
+                <LandingCounter count={parseInt(number)} /> {/* تبدیل رشته به عدد */}
                 +
             </h4>
             <p className='text-lg text-white font-bold'>{name}</p>
